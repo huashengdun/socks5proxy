6ee0e1de02a313426f0b7b0459a00aadeea1fa07
Sheng <webmaster0115@gmail.com>
2016-12-23 16:56:40 +0800

    Modulized server.py

--- server.py
+++ server.py
@@ -8,8 +8,6 @@ from common import (handle_tcp, parse_commands, ServerConfig,
 logger = logging.getLogger('server')
 
 CONNECT_TIMEOUT = 3
-ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-ssl_context.check_hostname = False
 
 
 @asyncio.coroutine
@@ -46,7 +44,18 @@ def serve(reader, writer):
         transport = yield from connect_to_dest(dest)
 
 
-def run():
+def main():
+    cmds = parse_commands(logger.name)
+    conf = ServerConfig(cmds)
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.check_hostname = False
+    ssl_context.load_cert_chain(conf.crt_file, conf.key_file)
+    log_cfg = get_logging_config(
+        log_level=conf.log_level, log_file=conf.log_file
+    )
+    logging.basicConfig(**log_cfg)
+    logger.debug(cmds)
+
     event_loop = asyncio.get_event_loop()
     factory = asyncio.start_server(
         serve, host=conf.server_ip, port=conf.server_port, ssl=ssl_context
@@ -69,12 +78,4 @@ def run():
 
 
 if __name__ == '__main__':
-    cmds = parse_commands(logger.name)
-    conf = ServerConfig(cmds)
-    ssl_context.load_cert_chain(conf.crt_file, conf.key_file)
-    log_cfg = get_logging_config(
-        log_level=conf.log_level, log_file=conf.log_file
-    )
-    logging.basicConfig(**log_cfg)
-    logger.debug(cmds)
-    run()
+    main()
